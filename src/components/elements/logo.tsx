007dba66564975b04dd66ff63e751ed737bddff9
kaduhiro <github@kaduhiro.com>
2023-01-17 21:04:23 +0900

    refactor: create element for logo

---
+++ src/components/elements/logo.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import { Props } from "@/types";
+import styles from "@/assets/css/DefaultLayout.module.css";
+
+export const Logo = () => {
+  return (
+    <div>
+      <a
+        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        By{" "}
+        <Image
+          src="/vercel.svg"
+          alt="Vercel Logo"
+          className={styles.vercelLogo}
+          width={100}
+          height={24}
+          priority
+        />
+      </a>
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 import { DefaultLayout } from "@/components/layouts";
 import styles from "@/assets/css/DefaultLayout.module.css";
+import { Logo } from "@/components/elements";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,23 +15,7 @@ export default function Home() {
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
           </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
+          <Logo />
         </div>
 
         <div className={styles.center}>
