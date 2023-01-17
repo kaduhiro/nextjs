a1b5ba918068dc744955b7294d128d3cea5f7981
kaduhiro <github@kaduhiro.com>
2023-01-17 21:01:57 +0900

    refactor: rename default css file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "@/assets/css/Home.module.css";
 import { DefaultLayout } from "@/components/layouts";
+import styles from "@/assets/css/DefaultLayout.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
