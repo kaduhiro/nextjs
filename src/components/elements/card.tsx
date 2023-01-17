e4235dd169cf8f08f0c9a68c552e0b3a4d350535
kaduhiro <github@kaduhiro.com>
2023-01-17 21:31:09 +0900

    refactor: create element for help

---
+++ src/components/elements/card.tsx
@@ -0,0 +1,29 @@
+import { Inter } from "@next/font/google";
+import { Props } from "@/types";
+import styles from "@/assets/css/DefaultLayout.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export type CardProps = {
+  url: string;
+  title: string;
+  description: string;
+};
+
+export const Card = ({ url, title, description }: CardProps) => {
+  return (
+    <>
+      <a
+        href={url}
+        className={styles.card}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <h2 className={inter.className}>
+          {title} <span>-&gt;</span>
+        </h2>
+        <p className={inter.className}>{description}</p>
+      </a>
+    </>
+  );
+};
---
+++ src/components/parts/help.tsx
@@ -0,0 +1,30 @@
+import { Props } from "@/types";
+import { Card } from "@/components/elements";
+import styles from "@/assets/css/DefaultLayout.module.css";
+
+export const Help = () => {
+  return (
+    <div className={styles.grid}>
+      <Card
+        url="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        title="Docs"
+        description="Find in-depth information about Next.js features and&nbsp;API."
+      />
+      <Card
+        url="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        title="Learn"
+        description="Learn about Next.js in an interactive course with&nbsp;quizzes!"
+      />
+      <Card
+        url="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        title="Templates"
+        description="Discover and deploy boilerplate example Next.js&nbsp;projects."
+      />
+      <Card
+        url="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        title="Deploy"
+        description="Instantly deploy your Next.js site to a shareable URL with&nbsp;Vercel."
+      />
+    </div>
+  );
+};
