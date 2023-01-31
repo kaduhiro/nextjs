8ee617de8eb3419cc456fd5c17e79e2aa5b65448
kaduhiro <github@kaduhiro.com>
2023-01-31 21:47:42 +0900

    style: format with prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
-import Image from "next/image";
-import { useRecoilState } from "recoil";
+import Image from 'next/image';
+import { useRecoilState } from 'recoil';
 
-import styles from "@/assets/css/Index.module.css";
-import { Logo } from "@/components/elements";
-import { DefaultLayout } from "@/components/layouts";
-import { Help } from "@/components/parts";
-import { counterState } from "@/states";
+import styles from '@/assets/css/Index.module.css';
+import { Logo } from '@/components/elements';
+import { DefaultLayout } from '@/components/layouts';
+import { Help } from '@/components/parts';
+import { counterState } from '@/states';
 
 export default function Home() {
   const [counter, setCounter] = useRecoilState(counterState);
@@ -18,35 +18,29 @@ export default function Home() {
 
   return (
     <DefaultLayout title={`Create Next App (${counter.count})`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <Logo />
-        </div>
+      <div className={styles.description}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>pages/index.tsx</code>
+        </p>
+        <Logo />
+      </div>
 
-        <div className={styles.center}>
-          <Image
-            className={"animate-slide-in-elliptic-top-fwd " + styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={"animate-bounce " + styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
+      <div className={styles.center}>
+        <Image
+          className={'animate-slide-in-elliptic-top-fwd ' + styles.logo}
+          src='/next.svg'
+          alt='Next.js Logo'
+          width={180}
+          height={37}
+          priority
+        />
+        <div className={'animate-bounce ' + styles.thirteen}>
+          <Image src='/thirteen.svg' alt='13' width={40} height={31} priority />
         </div>
+      </div>
 
-        <Help />
+      <Help />
     </DefaultLayout>
   );
 }
