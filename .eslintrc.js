3a950768c06b2628d05cc8c908bddc2f0daae2a5
kaduhiro <github@kaduhiro.com>
2023-02-02 21:13:11 +0900

    chore: change rules in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,47 @@
 module.exports = {
-  plugins: ["import", "unused-imports", "tailwindcss"],
-  extends: [
-    "next",
-    "next/core-web-vitals",
-    "plugin:tailwindcss/recommended",
-    "prettier",
-  ],
+  plugins: ['import', 'unused-imports', 'tailwindcss'],
+  extends: ['next', 'next/core-web-vitals', 'plugin:tailwindcss/recommended', 'prettier'],
   rules: {
-    "no-restricted-imports": ["error"],
-    "import/order": [
-      "error",
+    'no-restricted-imports': ['error'],
+    'import/order': [
+      'error',
       {
         alphabetize: {
-          order: "asc",
+          order: 'asc',
           caseInsensitive: true,
         },
-        groups: [
-          "builtin",
-          "external",
-          "parent",
-          "sibling",
-          "index",
-          "object",
-          "type",
-        ],
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'object', 'type', 'index'],
         pathGroups: [
           {
-            pattern: "{react,react-dom/**,react-router-dom}",
-            group: "builtin",
-            position: "before",
+            pattern: '{react,react-dom/**,react-router-dom}',
+            group: 'builtin',
+            position: 'before',
           },
           {
-            pattern: "@src/**",
-            group: "parent",
-            position: "before",
+            pattern: '{next/**}',
+            group: 'builtin',
+            position: 'before',
+          },
+          {
+            pattern: '@/models/**,@/repositories/**,@/usecases/**,@/components/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/states/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/assets/**',
+            group: 'index',
+            position: 'before',
           },
         ],
-        pathGroupsExcludedImportTypes: ["builtin"],
-        "newlines-between": "always",
+        pathGroupsExcludedImportTypes: ['builtin'],
+        'newlines-between': 'always',
       },
     ],
-    "unused-imports/no-unused-imports": "error",
+    'unused-imports/no-unused-imports': 'error',
   },
 };
