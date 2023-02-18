4e8e561307edd26687e7cf70d776b639efa64f43
kaduhiro <kaduhiro@users.noreply.github.com>
2023-02-18 12:35:03 +0000

    deploy: ba8502d536f4cd7706ee9b953e8c809c793cc537

---
+++ _next/static/b32wPPotMkpL6HQzVFeVD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/7a43bad66981e89d.css","static/chunks/pages/index-5b0af5ff2f04fda1.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
